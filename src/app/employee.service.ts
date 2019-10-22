e96a96b5f9e729396a9f68de0b09cfeab386ad52
Sujithnani.g <sujithnani.g@outlook.com>
2019-10-22 10:22:08 +0530

    api and bootstrap

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,39 +1,30 @@
 import { Injectable } from '@angular/core';
 import { IEmployee } from './employee/IEmployee';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+    constructor(private httpClient: HttpClient) { }
 
-  getEmployees():IEmployee[]{
-    return [
-      {
-          code: 'emp101', name: 'Tom', gender: 'Male',
-          annualSalary: 5500, dateOfBirth: '6/25/1988'
-      },
-      {
-          code: 'emp102', name: 'Alex', gender: 'Male',
-          annualSalary: 5700.95, dateOfBirth: '9/6/1982'
-      },
-      {
-          code: 'emp103', name: 'Mike', gender: 'Male',
-          annualSalary: 5900, dateOfBirth: '12/8/1979'
-      },
-      {
-          code: 'emp104', name: 'Mary', gender: 'Female',
-          annualSalary: 6500.826, dateOfBirth: '10/14/1980'
-      },
-      {
-          code: 'emp105', name: 'Nancy', gender: 'Female',
-          annualSalary: 6700.826, dateOfBirth: '12/15/1982'
-      },
-      {
-          code: 'emp106', name: 'Steve', gender: 'Male',
-          annualSalary: 7700.481, dateOfBirth: '11/18/1979'
-      },
-  ];
-  }
+    getEmployees(){
+
+
+        return this.httpClient.get<IEmployee[]>('http://localhost:50500/api/Employees')
+        // .pipe(
+        //     catchError(this.handleError)
+        // );;
+    }
+    handleError(error: IEmployee[]) {
+        console.error(error);
+        return Observable.throw(error);
+    }
 }
+
+
+
